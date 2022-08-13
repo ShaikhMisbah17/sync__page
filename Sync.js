c440d30349251f03fd4080e6678d106a61e8c990
MISBAH SHAIKH <96977835+ShaikhMisbah17@users.noreply.github.com>
2022-08-13 20:47:30 +0530

    sync updated work 13th Aug

--- Sync.js
+++ Sync.js
@@ -18,93 +18,100 @@ import Sydney from "../Assets/Sydney.png";
 import Brussels from "../Assets/Brussels.png";
 import LA from "../Assets/Los Angeles.png";
 
+const songs = new Array(12).fill(0).map((_, i) => ({
+  id: i + 1,
+  name: "Jugni",
+  duration: "4:42",
+  genre: "genre of song",
+}));
+
+const halfSongs = Math.ceil(songs.length / 2);
+
 function Sync() {
   return (
     <div className="sync">
-      <div className="sync__nav">
+      <div classname="sync__nav">
         <Navbar />
       </div>
-      <div className="sync__banner sync__banner-style">
-        <h2 className="banner__text1">
+      <div className="sync__banner">
+        <h2>
           In the end, it’s all about the
           <span className="banner__yellowtext">love of music</span>
         </h2>
-        <h4 className="banner__text2">
-          some of the music we are really proud of
-        </h4>
+        <h4>some of the music we are really proud of</h4>
       </div>
-      {/* Leftside */}
-      <div className="leftside">
-        <h3 className="project-showcase">Project Showcase</h3>
-        <div className="leftside__image-section1">
-          <img className="frame-images monsoon-img" src={monsoon} />
-          <img className="frame-images twilight-img" src={twilight} />
-          <img className="frame-images gift-img" src={gift} />
-          <img className="frame-images prague-img" src={prague} />
-          <img className="frame-images pune-img" src={pune} />
-        </div>
-        <div className="leftside__image-section2">
-          <img className="frame-images ncis-img" src={ncis} />
-          <img className="frame-images horn-img" src={horn} />
-          <img className="frame-images sunday-img" src={sunday} />
-          <img className="frame-images baji-img" src={baji} />
-          <img className="frame-images alternative-img" src={alternative} />
-        </div>
-      </div>
-      <hr className="sync__line horizontal-line"></hr>
-      <div className="rightside">
-        <div className="display__monsoon-image">
-          <img className="display-image" src={monsoon} />
-        </div>
-        <div className="display-image__description">
-          <h3>Monsoon Shootout</h3>
-          <img className="play-btn" src={play} />
-          <hr className="yellow-line"></hr>
-          <h5>Some comment on the project</h5>
-          <h4 className="description__details">Type: Feature Film</h4>
-          <h4 className="description__details">Release Year: 2017</h4>
-          <h4 className="description__details">Country: India</h4>
-          <h4 className="title__song">Songs</h4>
-          <div className="songs-list1">
-            <h3 className="song-name">
-              Jugni&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.42
-            </h3>
-            <h5>genre of song</h5>
-            <h3 className="song-name">
-              Jugni&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.42
-            </h3>
-            <h5>genre of song</h5>
-            <h3 className="song-name">
-              Jugni&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.42
-            </h3>
-            <h5>genre of song</h5>
+      <div className="wrapper">
+        <div className="left-section">
+          <h3 className="project-showcase">Project Showcase</h3>
+          <div className="leftside__image-section1">
+            <img className="frame-images monsoon-img" src={monsoon} />
+            <img className="frame-images twilight-img" src={twilight} />
+            <img className="frame-images gift-img" src={gift} />
+            <img className="frame-images prague-img" src={prague} />
+            <img className="frame-images pune-img" src={pune} />
           </div>
-          <hr className="list-line"></hr>
-          <div className="songs-list1 song-list-right">
-            <h3 className="song-name">
-              Jugni&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.42
-            </h3>
-            <h5>genre of song</h5>
-            <h3 className="song-name">
-              Jugni&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.42
-            </h3>
-            <h5>genre of song</h5>
-            <h3 className="song-name">
-              Jugni&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4.42
-            </h3>
-            <h5>genre of song</h5>
+          <div className="leftside__image-section2">
+            <img className="frame-images ncis-img" src={ncis} />
+            <img className="frame-images horn-img" src={horn} />
+            <img className="frame-images sunday-img" src={sunday} />
+            <img className="frame-images baji-img" src={baji} />
+            {/* <img className="frame-images alternative-img" src={alternative} /> */}
           </div>
         </div>
-        <hr className="scroll-bar scroll"></hr>
-      </div>
-      <hr className="sync__line-below"></hr>
-      <div className="sync__film-festival-section">
-        <h2>Film Festivals</h2>
-        <div className="film-frames">
-          <img src={Cannes} />
-          <img src={Sydney} />
-          <img src={Brussels} />
-          <img src={LA} />
+        <div className="divider"></div>
+        <div className="right-section">
+          <div className="right-section__top-wrapper">
+            <div className="col-1">
+              <img className="display-image" src={monsoon} />
+            </div>
+            <div className="col-2">
+              <div className="display-image__description">
+                <div className="row-alignment">
+                  <h3>Monsoon Shootout</h3>
+                  <img src={play} />
+                </div>
+                <hr className="yellow-line"></hr>
+                <h5>Some comment on the project</h5>
+                <h4 className="description__details">Type: Feature Film</h4>
+                <h4 className="description__details">Release Year: 2017</h4>
+                <h4 className="description__details">Country: India</h4>
+                <h4 className="title__song">Songs</h4>
+                <div className="songs-list">
+                  <div className="songs-list__left">
+                    {songs.slice(0, halfSongs).map((song) => (
+                      <div className="song-details">
+                        <div className="row-1">
+                          <p>{song.name}</p>
+                          <p>{song.duration}</p>
+                        </div>
+                        <p>{song.genre}</p>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="divider-2"></div>
+                  <div className="songs-list__right">
+                    {songs.slice(halfSongs, songs.length).map((song) => (
+                      <div className="song-details">
+                        <div className="row-1">
+                          <p>{song.name}</p>
+                          <p>{song.duration}</p>
+                        </div>
+                        <p>{song.genre}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr className="divider-3"></hr>
+          <h3 className="film-festival-section">Film Festivals</h3>
+          <div className="film-frames">
+            <img classname="film-img" src={Cannes} />
+            <img classname="film-img" src={Sydney} />
+            <img classname="film-img" src={Brussels} />
+            <img classname="film-img" src={LA} />
+          </div>
         </div>
       </div>
       <div className="footer">
